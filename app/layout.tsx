5347f8ddf44b01da532535d6092600292ee37def
ibizugbe <abrahamibizugbe@gmail.com>
2024-05-20 16:36:48 +0100

    Update layout to style body tag.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
 
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -17,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className={inter.className}>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
--- app/page.tsx
+++ app/page.tsx
@@ -6,16 +6,16 @@ import Image from "next/image";
 export default function Home() {
   return (
    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-    <div className="max-w-7xl w-full">
-      <FloatingNav className="" navItems={[
-        { name: "Home", link: "/" },
-        { name: "About", link: "/about" },
-        { name: "Projects", link: "/projects" },
-        { name: "Contact", link: "/contact" },
-      ]}/>
-      <Hero />
-      <Grid />
-    </div>
+      <div className="max-w-7xl w-full">
+        <FloatingNav className="" navItems={[
+          { name: "Home", link: "/" },
+          { name: "About", link: "/about" },
+          { name: "Projects", link: "/projects" },
+          { name: "Contact", link: "/contact" },
+        ]}/>
+        <Hero />
+        <Grid />
+      </div>
    </main>
   );
 }
